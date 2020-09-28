a98cdc7de498f4463dc24185d4cba0107f38d6ab
heojee1 <heojee1@gmail.com>
2020-09-29 03:54:17 +0900

    initial structure

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
+  });
