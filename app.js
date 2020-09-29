703a4f28eee7e08876976807da5b4184fd3aa59b
heojee1 <heojee1@gmail.com>
2020-09-29 17:01:17 +0900

    add mongoDB, add  show page to display description.

--- app.js
+++ app.js
@@ -1,40 +1,72 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose');
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-var campgrounds = [
-  {name: 'Mount Cook National Park', image: 'https://images.unsplash.com/photo-1511993807578-701168605ad3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
-  {name: 'Loch Lomond & The Trossachs', image: 'https://images.unsplash.com/photo-1488790881751-9068aa742b9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
-  {name: 'Sierra Nevada National Park', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
-  {name: 'Mount Cook National Park', image: 'https://images.unsplash.com/photo-1511993807578-701168605ad3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
-  {name: 'Loch Lomond & The Trossachs', image: 'https://images.unsplash.com/photo-1488790881751-9068aa742b9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
-  {name: 'Sierra Nevada National Park', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
-];
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
+
+/* SCHEMA setup */
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: 'Loch Lomond & The Trossachs', 
+//     image: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+//     description: 'Mersmerizing nature. Have an unforgettable stay with your favorite animal - quokkas'
+//   }, (err, campground) => {
+//     if (err) console.log(err);
+//     else console.log(campground);
+//   }
+// )
 
 /* get landing page */
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
-/* get campgrouds page */
+/* INDEX - show all campgrouds */
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', {campgrounds: campgrounds});
+  Campground.find({}, (err, campgrounds) => {
+    if(err) console.log(err);
+    else res.render('index', {campgrounds: campgrounds});
+  })
 });
 
-/* post a new campground */
+/* CREATE - add new campground to DB */
 app.post('/campgrounds', (req, res) => {
   var newCampground = {name: req.body.name, image: req.body.image};
-  campgrounds.push(newCampground);
-  res.redirect('/campgrounds');
+  Campground.create(newCampground, (err, campground) => {
+    if (err) console.log(err);
+    else res.redirect('/campgrounds');
+  });
 });
 
-/* get form to create a new campground */
+/* NEW - show form to create new campground */
 app.get('/campgrounds/new', (req, res) => {
   res.render('new');
+});
+
+/* SHOW - show more information about a campground */
+app.get('/campgrounds/:id', (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) console.log(err);
+    else res.render('show', {campground: foundCampground});
+  });
 });
 
 /* start the server */
